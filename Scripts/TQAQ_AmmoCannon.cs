0350f45483e2cf3c1209ad7f2becefe83e9faa52
AvenkyrPakacit <168223716+AvenkyrPakacit@users.noreply.github.com>
2025-11-03 01:15:14 -0600

    Cannon Ammo Hotfix
    Makes cannon ammo unique.

---
+++ Scripts/TQAQ_AmmoCannon.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class TQAQ_AmmoCannon : IPart
+    {
+        public string ProjectileObject;
+
+        public TQAQ_AmmoCannon()
+        {
+//            Name = "TQAQ_AmmoCannon";
+        }
+
+        public override bool SameAs(IPart p)
+        {
+            return true;
+        }
+
+        public override bool AllowStaticRegistration()
+        {
+            return true;
+        }
+
+        public override void Register(GameObject Object)
+        {
+            Object.RegisterPartEvent(this, "QueryEquippableList");
+            Object.RegisterPartEvent(this, "GetProjectileObject");
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "GetProjectileObject")
+            {
+                E.AddParameter("Projectile", GameObjectFactory.Factory.CreateObject(ProjectileObject));
+                return true;
+            }
+
+            if (E.ID == "QueryEquippableList")
+            {
+                if ((E.GetParameter("EquippableList") as List<GameObject>).CleanContains(ParentObject)) return true;
+                if ((E.GetParameter("SlotType") as string).Contains("TQAQ_AmmoCannon"))
+                {
+                    (E.GetParameter("EquippableList") as List<GameObject>).Add(ParentObject);
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return true;
+        }
+    }
+}
