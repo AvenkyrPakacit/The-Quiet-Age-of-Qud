deb09b5c2fb3b997af8611914f38fe3919e7c773
AvenkyrPakacit <pwthesnell2017@gmail.com>
2026-04-20 16:23:25 -0500

    Fixes to the main world.
    Now it works.

--- Scripts/TQAQ_Plane_Main_ZoneFactory.cs
+++ Scripts/TQAQ_Plane_Main_ZoneFactory.cs
@@ -1,5 +1,7 @@
+using XRL.World.ZoneFactories;
+
 namespace XRL.World.ZoneFactories {
-    public class TQAQ_Plane_Main_ZoneFactory : IZoneFactory {
+    public class TQAQ_Plane_Main_ZoneFactory : BlueprintZoneFactory {
         public override bool CanBuildZone(ZoneRequest Request) {
             // Use BuildZone only for the world map; otherwise, we use GenerateZone
             // and AddBlueprintsFor.
@@ -9,20 +11,10 @@ namespace XRL.World.ZoneFactories {
             var zone = new Zone(80, 25);
             zone.ZoneID = Request.ZoneID;
             if (Request.IsWorldZone) {
-                zone.ForeachCell(delegate(Cell c) {
-                    c.AddObject("TerrainJungle");
-                });
+            zone.loadMap("TQAQ_Raseke_WorldMap.rpm");
             }
             zone.DisplayName = "Raseke";
             return zone;
-        }
-        public override void AddBlueprintsFor(ZoneRequest Request) {
-            // Normally we would use the fields of the ZoneRequest to figure out
-            // what cell blueprint we should get. In this case, we just resort
-            // to using the same blueprint for all cells.
-            var cellBlueprint = Blueprint.CellBlueprintsByName["TQAQ_DefaultRasekeCell"];
-            var levelBlueprint = cellBlueprint.LevelBlueprint[1, 1, 10];
-            Request.Blueprints.Add(levelBlueprint);
         }
         public override void AfterBuildZone(Zone zone, ZoneManager zoneManager) {
             ZoneManager.PaintWalls(zone);
